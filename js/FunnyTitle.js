25786fc2c15fa665864d6bebd4ff1ecf1fa901e6
unknown <2361719482@qq.com>
2020-08-11 16:56:08 +0800

    Site updated: 2020-08-11 16:56:08

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,15 +1,15 @@
-// �������Ц����
+// 浏览器搞笑标题
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/funny.ico");
-        document.title = '�q(��A��`)�r ҳ������� ~';
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(?>��<*?) ���ֺ��� ~' + OriginTitle;
+        document.title = '(?>ω<*?) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
